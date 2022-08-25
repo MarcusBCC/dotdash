69e0c44b370adb359d3e38545f0cd498be765666
Marcus Benesch <marcus.benesch@beneschcyberconsulting.com>
2022-08-24 22:06:24 -0500

    initial repo

---
+++ src/components/SearchBox.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="card-form">
+    <h1>Book Review Search</h1>
+    <div class="card-input">
+      <input
+        type="text"
+        class="card-input__input"
+        placeholder="Search Term"
+        v-model="searchTerm"
+      />
+      <button class="card-form__button" @click="submitSearch">Search</button>
+
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "SearchBox",
+  components: {},
+  data() {
+    return {
+      searchTerm: "",
+    };
+  },
+  methods: {
+    submitSearch() {
+      this.$emit("search", this.searchTerm);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/SearchResult.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="card-form" style="margin-top: 10px">
+    <li class="list-group-item">
+    <div class = "row">
+        <div class = "col-2" style="margin-right:50px;">
+    <img :src="this.bookData.imageUrl">
+    </div>
+    <div class = "col">
+    <h2>{{this.bookData.title}}</h2>
+    <ul>
+        <li>Author: {{this.bookData.authorName}}</li>
+        <li>Rating: {{this.bookData.rating}}</li>
+        <li>Year Published: {{this.bookData.year}}</li>
+    </ul>
+    
+
+    </div>
+    </div>
+    </li>
+  </div>
+</template>
+
+<script>
+export default {
+    
+  name: "SearchResult",
+  components: {},
+  data() {
+    return {
+      
+    };
+  },
+  props: {
+    bookData:Object
+    
+  },
+  methods: {
+    
+    
+  },
+};
+</script>
+
+<style>
+</style>
