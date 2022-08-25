8307008f38e631dd7ef229476efcc42b692b4fb3
Marcus Benesch <marcus.benesch@beneschcyberconsulting.com>
2022-08-24 23:44:46 -0500

    vue config issues

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/dotdash/" : "/", 
+    publicPath: process.env.NODE_ENV === "production" ? "dotdash.github.io" : "/", 
   };
